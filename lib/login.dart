849144d94d9af33e6cdbbe12e8778f481a957844
Andrew Friedman <andrewfriedman01@gmail.com>
2022-04-13 22:01:24 -0400

    Add login page UI skeleton

--- lib/login.dart
+++ lib/login.dart
@@ -19,15 +19,101 @@ class _Login extends State<Login> {
             ),
             Column(
               children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.chevron_left,
-                    size: MediaQuery.of(context).size.width / 20,
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.chevron_left,
+                      size: MediaQuery.of(context).size.width / 20,
+                    ),
                   ),
-                )
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 3,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Image.asset(
+                            "assets/images/logo.png",
+                            height: MediaQuery.of(context).size.width / 5,
+                            width: MediaQuery.of(context).size.width / 5,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person),
+                            labelText: 'Email',
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.lock),
+                            labelText: 'Password',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        fontSize: MediaQuery.of(context).size.width / 61,
+                                        color: Colors.black
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Register",
+                                    style: TextStyle(
+                                        fontSize: MediaQuery.of(context).size.width / 61,
+                                        color: Colors.black
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Align(
+                            alignment: Alignment.centerRight,
+                            child: Text("Forgot password?")
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ]
             )
           ]
@@ -35,25 +121,4 @@ class _Login extends State<Login> {
       ),
     );
   }
-}
-
-Widget headerButton (var context, String text, void Function() action) {
-  return Expanded(
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: action,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            text,
-            style: TextStyle(
-                fontSize: MediaQuery.of(context).size.width / 50,
-                color: Colors.black
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
 }
