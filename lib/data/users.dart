d2102ecbeb81e5ce82a8bded11bf6aae5baf4b68
John Bagel <47508870+JohnLovesBagel@users.noreply.github.com>
2022-04-17 13:28:49 -0400

    Adding Table Template

---
+++ lib/data/users.dart
@@ -0,0 +1,22 @@
+import 'package:datatable_sortable_example/model/user.dart';
+
+final allUsers = <User>[
+  User(firstName: 'NC_045512', lastName: 'China', age: 2019),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2021),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2018),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2020),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2020),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2020),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2022),
+  User(firstName: 'NC_045512', lastName: 'China', age: 2019),
+];
