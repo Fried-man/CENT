28287f5d6188d5a5717a380929fa551a8d347dda
Andrew Friedman <andrewfriedman01@gmail.com>
2022-04-17 20:56:20 -0400

    Fix Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:genome_2133/tabs/saved.dart';
 
@@ -5,9 +6,16 @@ import 'home.dart';
 import 'tabs/login.dart';
 
 
-void main() {
-
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions( // TODO: import from index.html
+      apiKey: "AIzaSyBtGFwfR45P_psP3yYtibqlijhDJ0J02Oc",
+      appId: "1:29922646312:web:3d58b6360aa66789f2d81d",
+      messagingSenderId: "29922646312",
+      projectId: "genome-e2802",
+    ),
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
