6510b60014770797c84011babbc362081a8a58e0
Andrew Friedman <andrewfriedman01@gmail.com>
2022-11-08 18:17:00 -0500

    Add feature to save variant from variant card

--- lib/views/variant-card.dart
+++ lib/views/variant-card.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -33,9 +35,7 @@ class _VariantCard extends State<VariantCard> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-      child: ListView(
-        shrinkWrap: true,
-        primary: false,
+      child: Column(
         children: [
           Padding(
             padding: const EdgeInsets.only(top: 8),
@@ -83,6 +83,36 @@ class _VariantCard extends State<VariantCard> {
                     color: Colors.black),
               ),
             ),
+          ),
+          const Spacer(),
+          StreamBuilder<DocumentSnapshot>(
+            stream: FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return Container();
+
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: (){
+                    DocumentReference userDoc = FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid);
+                    if ((snapshot.data!["saved"] as List).contains(widget.variant["accession"])) {
+                      userDoc.update({'saved' : FieldValue.arrayRemove([widget.variant["accession"]])});
+                    }else {
+                      userDoc.update({'saved' : FieldValue.arrayUnion([widget.variant["accession"]])});
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      (snapshot.data!["saved"] as List).contains(widget.variant["accession"]) ? "Remove from Saved" : "Add to Saved",
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width / 100,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+              );
+            }
           ),
         ],
       ),
