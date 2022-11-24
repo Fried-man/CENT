a19e4f9b4406d8696f6ee5c0828344628fb63014
Andrew Friedman <andrewfriedman01@gmail.com>
2022-11-24 04:12:31 -0500

    Add loading indicator to variant card

--- lib/views/variant-card.dart
+++ lib/views/variant-card.dart
@@ -60,161 +60,172 @@ class _VariantCard extends State<VariantCard> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: FutureBuilder<Map<String, dynamic>>(
-          future: getVariants(widget.variant["accession"]),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Container();
-            if (snapshot.data!.containsKey("error")) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(child: Text(snapshot.data!["error"])),
-              );
-            }
-
-            return CustomScrollView(
-              shrinkWrap: true,
-              slivers: [
-                SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Column(
-                    children: [
-                      for (String key in snapshot.data!.keys)
-                        if (key != "Accession")
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: RichText(
-                              text: TextSpan(
-                                style: DefaultTextStyle.of(context).style,
-                                children: <TextSpan>[
-                                  TextSpan(text: key + ": ", style: const TextStyle(fontWeight: FontWeight.bold)),
-                                  if (snapshot.data![key] == "GenBank")
-                                    TextSpan(
-                                      text: snapshot.data![key],
-                                      style: TextStyle(color: Theme.of(context).scaffoldBackgroundColor), // decoration: TextDecoration.underline
-                                      recognizer: TapGestureRecognizer()
-                                        ..onTap = () { launchUrl(Uri.parse("https://www.ncbi.nlm.nih.gov/genbank/"));
-                                        },
-                                    ),
-                                  if (snapshot.data![key] != "GenBank")
-                                    TextSpan(text:
-                                    (key.contains("Date") ?
-                                    DateFormat("MMMM d, yyyy").format(DateTime.parse(snapshot.data![key].toString())) :
-                                    snapshot.data![key] != null && double.tryParse(snapshot.data![key].toString()) != null ?
-                                    snapshot.data![key].toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},') :
-                                    key.contains("Completeness") ?
-                                    snapshot.data![key].toString().toUpperCase() :
-                                    snapshot.data![key].toString())),
-                                ],
-                              ),
+      child: CustomScrollView(
+        shrinkWrap: true,
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                FutureBuilder<Map<String, dynamic>>(
+                    future: getVariants(widget.variant["accession"]),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Expanded(
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              color: Theme.of(context).scaffoldBackgroundColor,
                             ),
                           ),
-                      const Spacer(),
-                      Row(
+                        );
+                      }
+                      if (snapshot.data!.containsKey("error")) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(child: Text(snapshot.data!["error"])),
+                        );
+                      }
+
+                      return Column(
                         children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 3),
-                              child: ElevatedButton(
-                                style: TextButton.styleFrom(
-                                  textStyle: const TextStyle(fontSize: 20),
-                                ),
-                                onPressed: () => launchUrl(Uri.parse(
-                                    'https://www.ncbi.nlm.nih.gov/nuccore/' + widget.variant["accession"])),
-                                child: const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Open in NCBI",
-                                    maxLines: 1,
-                                    style: TextStyle(
-                                        fontSize: 15,
-                                        color: Colors.black),
+                          for (String key in snapshot.data!.keys)
+                            if (key != "Accession")
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: <TextSpan>[
+                                      TextSpan(text: key + ": ", style: const TextStyle(fontWeight: FontWeight.bold)),
+                                      if (snapshot.data![key] == "GenBank")
+                                        TextSpan(
+                                          text: snapshot.data![key],
+                                          style: TextStyle(color: Theme.of(context).scaffoldBackgroundColor), // decoration: TextDecoration.underline
+                                          recognizer: TapGestureRecognizer()
+                                            ..onTap = () { launchUrl(Uri.parse("https://www.ncbi.nlm.nih.gov/genbank/"));
+                                            },
+                                        ),
+                                      if (snapshot.data![key] != "GenBank")
+                                        TextSpan(text:
+                                        (key.contains("Date") ?
+                                        DateFormat("MMMM d, yyyy").format(DateTime.parse(snapshot.data![key].toString())) :
+                                        snapshot.data![key] != null && double.tryParse(snapshot.data![key].toString()) != null ?
+                                        snapshot.data![key].toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},') :
+                                        key.contains("Completeness") ?
+                                        snapshot.data![key].toString().toUpperCase() :
+                                        snapshot.data![key].toString())),
+                                    ],
                                   ),
                                 ),
                               ),
+                        ],
+                      );
+                    }
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 3),
+                        child: ElevatedButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () => launchUrl(Uri.parse(
+                              'https://www.ncbi.nlm.nih.gov/nuccore/' + widget.variant["accession"])),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Open in NCBI",
+                              maxLines: 1,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.black),
                             ),
                           ),
-                          if (FirebaseAuth.instance.currentUser == null)
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 3),
-                                child: ElevatedButton(
+                        ),
+                      ),
+                    ),
+                    if (FirebaseAuth.instance.currentUser == null)
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 3),
+                          child: ElevatedButton(
+                            onPressed: (){
+                              Navigator.pushNamed(context, '/login')
+                                  .then((value) => setState(() {}));
+                            },
+                            child: const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                "Login to Save",
+                                maxLines: 1,
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    if (FirebaseAuth.instance.currentUser != null)
+                      Expanded(child:
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3),
+                        child: StreamBuilder<DocumentSnapshot>(
+                            stream: FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).snapshots(),
+                            builder: (context, snapshot) {
+                              if (!snapshot.hasData) {
+                                return ElevatedButton(
                                   onPressed: (){
-                                    Navigator.pushNamed(context, '/login')
-                                        .then((value) => setState(() {}));
+                                    print("loading saved...");
                                   },
-                                  child: const Padding(
-                                    padding: EdgeInsets.all(8.0),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
                                     child: Text(
-                                      "Login to Save",
+                                      saveStatus,
                                       maxLines: 1,
-                                      style: TextStyle(
+                                      style: const TextStyle(
                                           fontSize: 15,
                                           color: Colors.black),
                                     ),
                                   ),
-                                ),
-                              ),
-                            ),
-                          if (FirebaseAuth.instance.currentUser != null)
-                            Expanded(child:
-                              Padding(
-                                padding: const EdgeInsets.only(left: 3),
-                                child: StreamBuilder<DocumentSnapshot>(
-                                  stream: FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).snapshots(),
-                                  builder: (context, snapshot) {
-                                    if (!snapshot.hasData) {
-                                      return ElevatedButton(
-                                        onPressed: (){
-                                          print("loading saved...");
-                                        },
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(
-                                            saveStatus,
-                                            maxLines: 1,
-                                            style: const TextStyle(
-                                                fontSize: 15,
-                                                color: Colors.black),
-                                          ),
-                                        ),
-                                      );
-                                    }
+                                );
+                              }
 
-                                    saveStatus = (snapshot.data!["saved"] as List).contains(widget.variant["accession"]) ? "Unsave" : "Add to Saved";
-                                    return ElevatedButton(
-                                      onPressed: (){
-                                        DocumentReference userDoc = FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid);
-                                        if ((snapshot.data!["saved"] as List).contains(widget.variant["accession"])) {
-                                          userDoc.update({'saved' : FieldValue.arrayRemove([widget.variant["accession"]])});
-                                        }else {
-                                          userDoc.update({'saved' : FieldValue.arrayUnion([widget.variant["accession"]])});
-                                        }
-                                      },
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          saveStatus,
-                                          maxLines: 1,
-                                          style: const TextStyle(
-                                              fontSize: 15,
-                                              color: Colors.black),
-                                        ),
-                                      ),
-                                    );
+                              saveStatus = (snapshot.data!["saved"] as List).contains(widget.variant["accession"]) ? "Unsave" : "Add to Saved";
+                              return ElevatedButton(
+                                onPressed: (){
+                                  DocumentReference userDoc = FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid);
+                                  if ((snapshot.data!["saved"] as List).contains(widget.variant["accession"])) {
+                                    userDoc.update({'saved' : FieldValue.arrayRemove([widget.variant["accession"]])});
+                                  }else {
+                                    userDoc.update({'saved' : FieldValue.arrayUnion([widget.variant["accession"]])});
                                   }
-                              ),
-                              )
-                            )
-                        ],
-                      ),
-
-                    ],
-                  ),
-                )
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    saveStatus,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                              );
+                            }
+                        ),
+                      )
+                      )
+                  ],
+                ),
               ],
-            );
-          }
-      ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
