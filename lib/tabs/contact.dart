2e42c81d0bc12c11d2867d6b46c2546923d9f51a
Andrew Friedman <andrewfriedman01@gmail.com>
2022-11-25 17:17:29 -0500

    Make contact us dynamic on signed in

--- lib/tabs/contact.dart
+++ lib/tabs/contact.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,7 +12,7 @@ final _formKey = GlobalKey<FormState>();
 final myController = TextEditingController();
 final myController2 = TextEditingController();
 
-Future sendEmail(String name, String email, String message) async {
+Future sendEmail(String email, String message, {String name = "John Doe"}) async {
   final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
   const serviceId = 'service_7dws4pq';
   const templateId = 'template_0u8p0b9';
@@ -45,24 +46,25 @@ AlertDialog contact(context) {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                      labelText: 'Your Email', prefixIcon: Icon(Icons.email)),
-                  validator: (emailValue) {
-                    if (emailValue == null ||
-                        emailValue.isEmpty ||
-                        !emailValue.contains('@') ||
-                        !emailValue.contains('.') ||
-                        emailValue.indexOf('.') == (emailValue.length - 1)) {
-                      return "Please enter a valid email address";
-                    }
-                    return null;
-                  },
+              if (FirebaseAuth.instance.currentUser == null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: myController,
+                    decoration: const InputDecoration(
+                        labelText: 'Your Email', prefixIcon: Icon(Icons.email)),
+                    validator: (emailValue) {
+                      if (emailValue == null ||
+                          emailValue.isEmpty ||
+                          !emailValue.contains('@') ||
+                          !emailValue.contains('.') ||
+                          emailValue.indexOf('.') == (emailValue.length - 1)) {
+                        return "Please enter a valid email address";
+                      }
+                      return null;
+                    },
+                  ),
                 ),
-              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
@@ -101,8 +103,11 @@ AlertDialog contact(context) {
               ),
               onPressed: () async {
                 if (_formKey.currentState!.validate()) {
-                  sendEmail(myController.value.text, myController.value.text,
-                      myController2.value.text);
+                  sendEmail(
+                      FirebaseAuth.instance.currentUser == null ? myController.value.text : FirebaseAuth.instance.currentUser!.email!,
+                      myController2.value.text,
+                      name: FirebaseAuth.instance.currentUser == null ? myController.value.text : FirebaseAuth.instance.currentUser!.uid,
+                  );
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(content: Text('Sending email...')),
                   );
