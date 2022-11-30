657b104c397b95662442cf93c43bc5f1ccfd694a
Andrew Friedman <andrewfriedman01@gmail.com>
2022-11-30 00:28:49 -0500

    Configure firebase & fix MacOS

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:genome_2133/tabs/saved.dart';
 
+import 'firebase_options.dart';
 import 'home.dart';
 import 'tabs/login.dart';
 
@@ -12,13 +13,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      // TODO: import from index.html
-      apiKey: "AIzaSyBtGFwfR45P_psP3yYtibqlijhDJ0J02Oc",
-      appId: "1:29922646312:web:3d58b6360aa66789f2d81d",
-      messagingSenderId: "29922646312",
-      projectId: "genome-e2802",
-    ),
+    name: "CENT",
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
