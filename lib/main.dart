46f7f30537e867f07c7119e6a14cffa6c58ae40a
Andrew Friedman <andrewfriedman01@gmail.com>
2022-12-05 11:11:48 -0500

    Fix more colors

--- lib/main.dart
+++ lib/main.dart
@@ -85,15 +85,14 @@ class _MyAppState<T> extends State<MyApp> {
       ),
       "Dark Mode" :
       ThemeData(
-          primarySwatch: Colors.grey,
+          primarySwatch: buildMaterialColor(Colors.white24),
           dialogBackgroundColor: const Color(0xff3d3d3d),
           scaffoldBackgroundColor: Colors.black87,
           secondaryHeaderColor: const Color(0xff445756),
           cardColor: const Color(0xff2B3E3D),
           textTheme: isDyslexic ? GoogleFonts.lexendDecaTextTheme() : GoogleFonts.nunitoSansTextTheme(),
-          primaryColor: Colors.white,
+          primaryColor: Colors.white60,
           highlightColor: Colors.blue,
-          primaryColorLight: Colors.white,
           brightness: Brightness.dark,
       ),
     };
@@ -149,4 +148,24 @@ class Illegal extends StatelessWidget {
         )
     );
   }
+}
+
+MaterialColor buildMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map<int, Color> swatch = {};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
 }
